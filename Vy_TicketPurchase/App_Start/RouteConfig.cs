26d84dd44717a8eb18b1add56813ca0b5ee7b69d
Unknown <martinarebekka@gmail.com>
2019-09-19 13:16:23 +0200

    Opprettet tom controller og oppdatert navn i routeconfig

--- Vy_TicketPurchase/App_Start/RouteConfig.cs
+++ Vy_TicketPurchase/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Vy_TicketPurchase
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Vy", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
