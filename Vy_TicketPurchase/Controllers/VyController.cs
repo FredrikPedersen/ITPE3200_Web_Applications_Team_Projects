109239252c39ba685b7c64490cd5891fdbfd5f4b
Unknown <martinarebekka@gmail.com>
2019-09-19 13:16:23 +0200

    Created empty controller and updated routeconfig

---
+++ Vy_TicketPurchase/Controllers/VyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vy_TicketPurchase.Controllers
+{
+    public class VyController : Controller
+    {
+        // GET: Vy
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
