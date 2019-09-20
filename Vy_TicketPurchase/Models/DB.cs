a46ac90b939ee010ceb84366253e7b2149417836
Unknown <martinarebekka@gmail.com>
2019-09-20 10:57:44 +0200

    Created database

--- Vy_TicketPurchase/Models/DB.cs
+++ Vy_TicketPurchase/Models/DB.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
 namespace Vy_TicketPurchase.Models
 {
-    public class DB
+    public class DB : DbContext
     {
+        public DB() : base("name=Bestilling")
+        {
+            Database.CreateIfNotExists();
+            //Database.SetInitializer(new DBInit());
+            //for å sette inn forhåndslaget data?, krever en DBInit klasse
+        }
+        public DbSet<Ticket> Billetter { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
