d4f3b2255987fa392b88fa1c13dff26475db6df0
Unknown <martinarebekka@gmail.com>
2019-09-20 11:56:45 +0200

    Created IndexView with drop down list

--- Vy_TicketPurchase/Models/Route.cs
+++ Vy_TicketPurchase/Models/Route.cs
@@ -7,5 +7,10 @@ namespace Vy_TicketPurchase.Models
 {
     public class Route
     {
+        public int id { get; set; }
+        public String startlocation { get; set; }
+        public String stoplocation { get; set; }
+        public double price { get; set; }
+        public String travelTime { get; set; }
     }
 }
