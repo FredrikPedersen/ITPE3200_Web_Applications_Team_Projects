97932c940dcede0ba28f621650f73a66d84287f7
Unknown <martinarebekka@gmail.com>
2019-09-20 10:39:05 +0200

    Created ticketclass

---
+++ Vy_TicketPurchase/Models/Billett.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vy_TicketPurchase.Models
+{
+    public class Billett
+    {
+        public String strekning { get; set; }
+        public String tidspunkt { get; set; }
+
+    }
+}
---
+++ Vy_TicketPurchase/Models/DB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vy_TicketPurchase.Models
+{
+    public class DB
+    {
+    }
+}
