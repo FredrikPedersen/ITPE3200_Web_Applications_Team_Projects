ffd690b0820697d5c35d8e7bc7777601c7c67ca5
Fredrik Pedersen <fredrikhp@gmail.com>
2019-09-20 18:32:34 +0200

    Created template for DBInit class with one Route added

---
+++ Vy_TicketPurchase/Models/DBInit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Vy_TicketPurchase.Models
+{
+    public class DBInit : DropCreateDatabaseAlways<DB>
+    {
+        protected override void Seed(DB context)
+        {
+            var route1 = new Route()
+            {
+                startlocation = "Oslo S",
+                stoplocation = "Lillestrøm",
+                price = 30,
+                travelTimeMinutes = 15,
+            };
+
+            context.Routes.Add(route1);
+            base.Seed(context);
+        }
+    }
+}
