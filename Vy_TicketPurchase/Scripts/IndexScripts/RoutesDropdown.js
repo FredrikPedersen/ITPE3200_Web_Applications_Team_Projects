f1c435c32010a285b443a2f22b2ec6699be24352
Fredrik Pedersen <fredrikhp@gmail.com>
2019-09-23 17:53:12 +0200

    Implemented script for adding all Routes in the database to a dropdown menu

---
+++ Vy_TicketPurchase/Scripts/IndexScripts/RoutesDropdown.js
@@ -0,0 +1,19 @@
+﻿$.ajax({
+    url: '/Vy/GetAllRoutes',
+    type: 'GET',
+    dataType: 'json',
+    success: function(jsRoute) {
+        ShowDropdown(jsRoute);
+    },
+    error: function() {
+        alert("Something something handle this later") //TODO Handle this later
+    }
+});
+
+function ShowDropdown(jsRoutes) {
+    var outString = "";
+    for (var i in jsRoutes) {
+        outString += "<option value='" + jsRoutes[i].id + "'>" + jsRoutes[i].routeName+"</option>";
+    }
+    $("#routes").append(outString);
+}
