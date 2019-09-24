4c378e1dd5fe46b020a095e9f7b6f55a5b95ec0f
Fredrik Pedersen <fredrikhp@gmail.com>
2019-09-24 14:57:44 +0200

    Renamed ShowDropdown to FillDropdown. Commenting.

--- Vy_TicketPurchase/Scripts/IndexScripts/RoutesDropdown.js
+++ Vy_TicketPurchase/Scripts/IndexScripts/RoutesDropdown.js
@@ -1,16 +1,18 @@
-﻿$.ajax({
+﻿//Get a list of all available routes from the controller and fill them into a dropdown-list
+$.ajax({
     url: '/Vy/GetAllRoutes',
     type: 'GET',
     dataType: 'json',
     success: function(jsRoute) {
-        ShowDropdown(jsRoute);
+        FillDropdown(jsRoute);
     },
     error: function() {
         alert("Something went wrong when trying to load Routes from the database") //TODO find a better way to handle this
     }
 });
 
-function ShowDropdown(jsRoutes) {
+//Iterates through all jsRoute-objects and puts their Routenames in as options in  a dropdown
+function FillDropdown(jsRoutes) {
     var outString = "";
     for (var i in jsRoutes) {
         outString += "<option value='" + jsRoutes[i].Id + "'>" + jsRoutes[i].RouteName+"</option>";
