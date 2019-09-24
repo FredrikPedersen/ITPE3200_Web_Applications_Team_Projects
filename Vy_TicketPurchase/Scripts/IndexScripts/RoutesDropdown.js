eeacea2936f6368418ffcc6607cc0db21dfd1da6
Fredrik Pedersen <fredrikhp@gmail.com>
2019-09-24 15:49:11 +0200

    Added functionality for showing info about a selected route

--- Vy_TicketPurchase/Scripts/IndexScripts/RoutesDropdown.js
+++ Vy_TicketPurchase/Scripts/IndexScripts/RoutesDropdown.js
@@ -1,14 +1,34 @@
-﻿//Get a list of all available routes from the controller and fill them into a dropdown-list
-$.ajax({
-    url: '/Vy/GetAllRoutes',
-    type: 'GET',
-    dataType: 'json',
-    success: function(jsRoute) {
-        FillDropdown(jsRoute);
-    },
-    error: function() {
-        alert("Something went wrong when trying to load Routes from the database") //TODO find a better way to handle this
-    }
+﻿$(function () {
+
+    //Get a list of all available routes from the controller and fill them into a dropdown-list
+    $.ajax({
+        url: '/Vy/GetAllRoutes/',
+        type: 'GET',
+        dataType: 'json',
+        success: function(jsRoute) {
+            FillDropdown(jsRoute);
+        },
+        error: function() {
+            alert("Something went wrong when trying to load Routes from the database") //TODO find a better way to handle this
+        }
+    });
+    
+    //Whenever the selected item in the routes dropdown is changed, get and display info about selected route
+    $("#routes").change(function () {
+        alert("Change function is called");
+        var id = $(this).val();
+        $.ajax({
+            url: '/Vy/GetRouteInfo/' + id,
+            type: 'GET',
+            dataType: 'json',
+            success: function (route) {
+                ShowInfo(route);
+            },
+            error: function () {
+                alert("Something went wrong when trying to load Routes from the database");
+            }
+        });
+    });
 });
 
 //Iterates through all jsRoute-objects and puts their Routenames in as options in  a dropdown
@@ -18,4 +38,11 @@ function FillDropdown(jsRoutes) {
         outString += "<option value='" + jsRoutes[i].Id + "'>" + jsRoutes[i].RouteName+"</option>";
     }
     $("#routes").append(outString);
+}
+
+//Formats info from passed route and displays it in the view
+function ShowInfo(route) {
+    var outString = "Travel time: " + route.TravelTimeMinutes + "m. Price: "
+        + route.Price + "NOK";
+    $("#showInfo").html(outString);
 }
