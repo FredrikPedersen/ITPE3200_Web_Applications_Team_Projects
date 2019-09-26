843a23a2b20561c4cd5704e8b8a8b7ed6a4903c6
FredrikPedersen <fredrikhp@gmail.com>
2019-09-26 19:09:37 +0200

    Removed method used for testing purposes

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Models/DBModels/Route.cs
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Models/DBModels/Route.cs
@@ -7,10 +7,5 @@
         public string Stoplocation { get; set; }
         public double Price { get; set; }
         public int TravelTimeMinutes { get; set; }
-
-        public override string ToString()
-        {
-            return Startlocation + " " + Stoplocation + " " + Price + " " + TravelTimeMinutes;
-        }
     }
 }
