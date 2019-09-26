1ecb5446cd9d2cb733a5c8a6a04cc5fbc1c2e6ab
FredrikPedersen <fredrikhp@gmail.com>
2019-09-26 19:11:04 +0200

    Renamed variables for consistency

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Models/DBModels/DBInit.cs
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Models/DBModels/DBInit.cs
@@ -32,8 +32,8 @@ namespace Vy_TicketPurchase_Core.Models.DBModels
                         var columns = line.Split("|");
                         var aRoute = new Route
                         {
-                            Startlocation = columns[0],
-                            Stoplocation = columns[1],
+                            StartLocation = columns[0],
+                            StopLocation = columns[1],
                             TravelTimeMinutes = Int32.Parse(columns[2]),
                             Price = Double.Parse(columns[3])
                         };
