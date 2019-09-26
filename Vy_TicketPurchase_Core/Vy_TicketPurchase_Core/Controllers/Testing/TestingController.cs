1b3f86ca91024ff8a579abf8af1adf637eda5771
FredrikPedersen <fredrikhp@gmail.com>
2019-09-26 19:58:19 +0200

    Added testing view and controller

---
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Controllers/Testing/TestingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vy_TicketPurchase_Core.Controllers.Testing
+{
+    public class TestingController : Controller
+    {
+        public ActionResult Testing()
+        {
+            return View();
+        }
+    }
+}
