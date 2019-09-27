e30cb533a8d44d80211581ed58a1a675c396aade
Unknown <martinarebekka@gmail.com>
2019-09-27 12:33:49 +0200

    opdated layout

---
+++ Vy_BillettKjøp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vy_BillettKjøp.MvcApplication" Language="C#" %>
--- Vy_TicketPurchase/Controllers/VyController.cs
+++ Vy_TicketPurchase/Controllers/VyController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
-using Vy_TicketPurchase.Models;
 using Vy_TicketPurchase.Models.DBModels;
 using Vy_TicketPurchase.Models.DomainModels;
 
