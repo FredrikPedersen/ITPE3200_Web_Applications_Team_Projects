11d143da09f26669c35b272795426e926b015be2
Fredrik Pedersen <fredrikhp@gmail.com>
2019-09-30 12:46:28 +0200

    Added method for retrieving all tickets from the database

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Services/Tickets/TicketService.cs
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Services/Tickets/TicketService.cs
@@ -1,4 +1,8 @@
-﻿using Vy_TicketPurchase_Core.Models.DBModels;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Vy_TicketPurchase_Core.Models.DBModels;
+using Vy_TicketPurchase_Core.Services.Stations.Models;
+using Vy_TicketPurchase_Core.Services.Tickets.Models;
 
 namespace Vy_TicketPurchase_Core.Services.Tickets
 {
@@ -9,6 +13,18 @@ namespace Vy_TicketPurchase_Core.Services.Tickets
         public TicketService(DatabaseContext dbContext)
         {
             _databaseContext = dbContext;
+        }
+        
+        public List<ServiceModelTicket> GetAllTickets()
+        {
+            return _databaseContext.Tickets.Select(t => new ServiceModelTicket
+            {
+                Id = t.Id,
+                FromStation = t.DbFromStation,
+                ToStation = t.DbToStation,
+                Customer = t.DbCustomer,
+                ValidFrom = t.ValidFrom
+            }).ToList();
         }
     }
 }
