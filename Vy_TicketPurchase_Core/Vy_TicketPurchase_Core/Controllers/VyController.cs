cfd0c8fa66998f118bb7e85dcd0eb08db885f075
Unknown <martinarebekka@gmail.com>
2019-09-30 10:43:41 +0200

    Merged changes into master

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Controllers/VyController.cs
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Controllers/VyController.cs
@@ -13,5 +13,14 @@ namespace Vy_TicketPurchase_Core.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(Services.Tickets.Models.ServiceModelTicket ticket) //kommer inn i metoden, men vet ikke hva slags objekt som må være her, kanskje lage et nytt med alle attributtene?
+        {
+            Console.WriteLine("linje før");
+            Console.WriteLine(ticket);
+            Console.WriteLine("linje etter");
+            return View();
+            //return RedirectToAction();
+        }
     }
 }
