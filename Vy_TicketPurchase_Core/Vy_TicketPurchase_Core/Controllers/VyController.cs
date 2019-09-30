54912a57f470c73e239b4989fde29e749a13ab04
Unknown <martinarebekka@gmail.com>
2019-09-30 13:22:53 +0200

    Created objects in controller that can be saved to the database

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Controllers/VyController.cs
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Controllers/VyController.cs
@@ -16,11 +16,19 @@ namespace Vy_TicketPurchase_Core.Controllers
         [HttpPost]
         public ActionResult Index(Services.Tickets.Models.ServiceModelTicket ticket) //kommer inn i metoden, men vet ikke hva slags objekt som må være her, kanskje lage et nytt med alle attributtene?
         {
-            Console.WriteLine("linje før");
-            Console.WriteLine(ticket);
-            Console.WriteLine("linje etter");
-            return View();
-            //return RedirectToAction();
+            DbCustomer customer = new DbCustomer {
+                Name = ticket.CustomerName,
+                Phonenumber = ticket.CustomerNumber
+            };
+            DbTicket newTicket = new DbTicket
+            {
+                //stasjon til og fra objekt
+                ValidFrom = ticket.ValidFrom,
+                DbCustomer = customer
+            };
+            //her kommer kode for å legge inn i databasen
+            //må opprette eget customer og ticketobjekt før det lagres i databasen
+            return RedirectToAction("ListView"); //må lage listeview
         }
     }
 }
