5dcfb8dc84d93df35597fad6a4b9092046701f72
Unknown <martinarebekka@gmail.com>
2019-10-01 16:42:14 +0200

    Autocomplete is working!

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/wwwroot/js/site.js
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    $(document).ready(function () {
+        $(".autocomplete").autocomplete({
+            source: function (request, response) {
+                $.ajax({
+                    url: "/Vy/Autocomplete",
+                    type: "POST",
+                    datatype: "json",
+                    data: { input: request.term },
+                    success: function (data) {
+                        response($.map(data, function (item) {
+                            return { label: item, value: item };
+                        }))
+
+                    }
+                })
+            },
+        });
+    })
