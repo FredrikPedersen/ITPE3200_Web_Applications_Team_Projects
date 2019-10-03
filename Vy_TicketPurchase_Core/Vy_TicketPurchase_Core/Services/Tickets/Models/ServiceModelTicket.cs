d9c2be4e3e35252da8b9f82f3aeb6633ba062a90
Unknown <martinarebekka@gmail.com>
2019-10-03 13:28:07 +0200

    Small changes, not complete validation until date and time object has been split.

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Services/Tickets/Models/ServiceModelTicket.cs
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Services/Tickets/Models/ServiceModelTicket.cs
@@ -13,12 +13,13 @@ namespace Vy_TicketPurchase_Core.Services.Tickets.Models
         [Required(ErrorMessage = "Frastasjon må velges")]
         [DisplayName("Fra")]
         public string FromStation { get; set; }
-        [Required(ErrorMessage = "Tilstajon må velges")]
+        [Required(ErrorMessage = "Tilstasjon må velges")]
         [DisplayName("Til")]
         public string ToStation { get; set; }
         [DisplayName("Avreise")]
         [DataType(DataType.DateTime)]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        [RegularExpression(@"(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.(19|20)\d\d", ErrorMessage = "Feil format på dato")] 
         public DateTime ValidFrom { get; set; }
         [Required(ErrorMessage ="Navn må fylles ut")]
         [DisplayName("Fullt navn")]
