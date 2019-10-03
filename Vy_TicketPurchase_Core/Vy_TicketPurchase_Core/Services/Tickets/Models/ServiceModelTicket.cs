c1780067371e468e66c77fdd72106dde542e9657
Unknown <martinarebekka@gmail.com>
2019-10-03 12:27:30 +0200

    Added validation in the serviceticket class

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Services/Tickets/Models/ServiceModelTicket.cs
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Services/Tickets/Models/ServiceModelTicket.cs
@@ -10,14 +10,20 @@ namespace Vy_TicketPurchase_Core.Services.Tickets.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Frastasjon må velges")]
         [DisplayName("Fra")]
         public string FromStation { get; set; }
+        [Required(ErrorMessage = "Tilstajon må velges")]
         [DisplayName("Til")]
         public string ToStation { get; set; }
+        [Required(ErrorMessage = "Dato må velges")]
         [DisplayName("Avreise")]
         public DateTime ValidFrom { get; set; }
+        [Required(ErrorMessage ="Navn må fylles ut")]
         [DisplayName("Fullt navn")]
         public string CustomerName { get; set; }
+        [RegularExpression(@"[0-9]{8}", ErrorMessage ="Telefonnummer må inneholde 8 siffer")]
+        [Required(ErrorMessage = "Telefonnummer må oppgis")]
         [DisplayName("Telefonnummer")]
         public string CustomerNumber { get; set; }
     }
