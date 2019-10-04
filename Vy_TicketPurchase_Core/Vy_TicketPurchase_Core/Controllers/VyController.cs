8ce7f3639012185b1966fd9c524b8ec89c923f51
Unknown <martinarebekka@gmail.com>
2019-10-04 15:19:59 +0200

    Started validating from and tostation

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Controllers/VyController.cs
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Controllers/VyController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Vy_TicketPurchase_Core.Models.DBModels;
 using Vy_TicketPurchase_Core.Services.Stations;
+using Vy_TicketPurchase_Core.Services.Stations.Models;
 using Vy_TicketPurchase_Core.Services.Tickets;
 using Vy_TicketPurchase_Core.Services.Tickets.Models;
 
@@ -19,29 +20,46 @@ namespace Vy_TicketPurchase_Core.Controllers
             _ticketService = ticketService;
             _stationService = stationService;
         }
-        
-        public ActionResult Index() 
+
+        public ActionResult Index()
         {
             return View();
         }
-        
+
         [HttpPost]
-        public ActionResult Index(ServiceModelTicket ticket) 
-        { 
+        public ActionResult Index(ServiceModelTicket ticket)
+        {
+            bool isValidFromStation = false;
+            bool isValidToStation = false;
             if (ModelState.IsValid)
             {
-                _ticketService.SaveTicket(ticket);
-                return RedirectToAction("List", "List");
+                foreach (var station in _stationService.GetAllStations())
+                {
+                    if (ticket.FromStation == station.StationName)
+                    {
+                        isValidFromStation = true;
+                    }
+                    if (ticket.ToStation == station.StationName)
+                    {
+                        isValidToStation = true;
+                    }
+                }
+                if (isValidToStation && isValidFromStation)
+                {
+                    _ticketService.SaveTicket(ticket);
+                    return RedirectToAction("List", "List");
+                }
             }
+            //kode som viser feilmelding her
             return View();
         }
-        
+
         //Calls autocomplete method for "From" text box in Index View
         public JsonResult Autocomplete(string input)
         {
             return Json(_stationService.ServiceAutocomplete(input));
         }
-        
+
         //Calls autocomplete method for "To" text box in Index View
         public JsonResult AutocompleteTo(string input, string fromStation)
         {
