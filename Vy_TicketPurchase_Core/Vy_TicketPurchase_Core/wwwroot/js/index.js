6c9e10652c90ca075e9f3c9cdb8d1f6a0e24c227
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-06 15:21:59 +0200

    Fixed a bug with the JavaScript validation where we check if the user has selected two different stations

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/wwwroot/js/index.js
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/wwwroot/js/index.js
@@ -1,33 +1,31 @@
 //Initialize variables that will be used in the script
 var from = document.querySelector('#from');
 var to = document.querySelector('#to');
-var errMsg = document.querySelector('#test-err');
-var fromInput = from.value;
+var errMsg = document.querySelector('#same-station-err');
 var btnInput = document.querySelector('#btn-submit');
 var hiddenDiv = document.querySelector('#hidden-div');
 var hideButton = document.querySelector('#hide-div');
-console.log(fromInput);
 
-//Add an event listener to the submitt button
+//Eventlisteners
 btnInput.addEventListener('click', checkInput);
-
 from.addEventListener('click', showElements);
 to.addEventListener('click', showElements);
 hideButton.addEventListener('click', hideDiv);
 
-//Checks input in from and to text boxes and stops the submittion procces if the contents are equal
-function checkInput(e) {
+//Checks input in from and to text boxes and stops the submit process if the contents are equal
+function checkInput(event) {
     var fromInput = from.value;
     var toInput = to.value;
-    if(fromInput === toInput) {
-        e.preventDefault();
+    var stringComparison = toInput.toString().localeCompare(fromInput.toString());
+    var booleanComparison = stringComparison === 0;
+    if(booleanComparison) {
+        event.preventDefault();
         errMsg.classList.add('error');
-        errMsg.innerHTML = "Du må velge to forskjellige stasjoner!"
+        errMsg.innerHTML = "Du må velge to forskjellige stasjoner!";
     }
 }    
     
 function showElements() {
-    console.log("click");
     hiddenDiv.classList.remove('hidden');
     hideButton.classList.remove('hidden');
 }
