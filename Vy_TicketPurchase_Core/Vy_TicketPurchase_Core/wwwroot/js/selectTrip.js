43dac81675e66941836d3f657669b3ecb6799dfe
Unknown <martinarebekka@gmail.com>
2019-10-17 15:46:29 +0200

    javascriptissues

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/wwwroot/js/selectTrip.js
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/wwwroot/js/selectTrip.js
@@ -1,23 +1,10 @@
-﻿var time1 = document.querySelector('#1');
-var time2 = document.querySelector('#2');
-var time3 = document.querySelector('#3');
-var time4 = document.querySelector('#4');
+﻿var time1 = document.querySelector('#en');
+var time2 = document.querySelector('#to');
+var time3 = document.querySelector('#tre');
+var time4 = document.querySelector('#fire');
 
-function setTimeFrom(event) {
-    var oldonload = window.onload;
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    } else {
-        window.onload = function () {
-            if (oldonload) {
-                oldonload();
-            }
-            event();
-        }
-    }
-}
+console.log(time1.value)
 
-addLoadEvent(function () {
-    document.getElementById('lbltipAddedComment').innerHTML = 'your tip has been submitted!';
-});
+function setTimeFrom() {
+    console.log(time2)
 }
