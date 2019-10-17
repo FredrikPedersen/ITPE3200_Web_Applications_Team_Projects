fdbc1b106d9190453378f3ba60e9e7178b1e5b74
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-17 12:09:54 +0200

    Added NumberOnLine attribute in Stations

--- Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Repository/DBModels/DbStation.cs
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Repository/DBModels/DbStation.cs
@@ -6,6 +6,7 @@ namespace Vy_TicketPurchase_Core.Repository.DBModels
     {
         [Key] 
         public int Id { get; set; }
+        public int NumberOnLine { get; set; }
         public string StationName { get; set; }
         public virtual DbTrainLine TrainLine { get; set; }
     }
