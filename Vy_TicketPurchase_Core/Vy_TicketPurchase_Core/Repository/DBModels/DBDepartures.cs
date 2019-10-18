1d30dbdd9333c852e16762c45a60e6856b0a3e54
Unknown <martinarebekka@gmail.com>
2019-10-18 13:37:41 +0200

    Created DbDepartureClass containing comments for full implementation guide to the rest of departureimplementation.

---
+++ Vy_TicketPurchase_Core/Vy_TicketPurchase_Core/Repository/DBModels/DBDepartures.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vy_TicketPurchase_Core.Repository.DBModels
+{
+    public class DBDepartures
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public string departureDate { get; set; }
+
+        [DataType(DataType.Time)]
+        public string departureTime { get; set; }
+
+        //Kan potensielt legge inn stasjoner og linjer i tillegg her for å få egenspesifiserte avgangstider per stasjon ved utvidelse
+        //Trenger en seedmetode som putter avganger inn i databasen
+        //Må legge inn departures som en tabell i databasen
+        //Hente ut alle filer i avgangdatabasen til avganger med knapp for å velge avgang, som er etter valgt tidspunkt( metode i ny DepartureService klasse under business)
+        //Trenger kun servicemodelDeparture for å redigere avganger i et eget view til innlevering 2
+        //Fra forrige view, får inn en servicemodelticket, oppdaterer tid og dato verdiene og sender videre for å opprette objektet i kontrolleren.
+    }
+}
