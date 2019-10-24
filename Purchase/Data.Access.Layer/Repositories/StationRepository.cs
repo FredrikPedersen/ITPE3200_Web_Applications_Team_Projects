e609ece1c036eb201fa0de053be16bb9d030e6a8
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-24 17:58:12 +0200

    Fixed references

--- Purchase/Data.Access.Layer/Repositories/StationRepository.cs
+++ Purchase/Data.Access.Layer/Repositories/StationRepository.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Purchase.Data.Access.Layer;
 using Purchase.Model.DBModels;
 using Purchase.Model.RepositoryModels;
 
