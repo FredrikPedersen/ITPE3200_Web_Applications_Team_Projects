08479c2874e7c5f43e6f446552b88fd9224b1642
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-25 18:28:11 +0200

    Renamed Security to Utilities

--- Purchase/MVC/Controllers/VyController.cs
+++ Purchase/MVC/Controllers/VyController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Model.DBModels;
 using Model.RepositoryModels;
-using Security.Passwords;
+using Utilities.Passwords;
 
 namespace MVC.Controllers
 {
