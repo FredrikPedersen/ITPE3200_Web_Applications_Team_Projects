def9971f22e4bc4a9f73a16f5301621d923cd9ae
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-26 15:41:08 +0200

    Added example ticket to prevent program crash when navigating to the ticket viee

--- Purchase/MVC/Controllers/TicketController.cs
+++ Purchase/MVC/Controllers/TicketController.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Logic.Layer;
 using Microsoft.AspNetCore.Mvc;
+using Model.RepositoryModels;
 using Model.ViewModels;
 
 namespace MVC.Controllers
@@ -17,7 +18,22 @@ namespace MVC.Controllers
         {
 
             var ticketList = TicketBll.GetAllTickets();
-            var model = ticketList[ticketList.Count - 1];
+            RepositoryModelTicket model;
+
+            if (ticketList.Count == 0)
+                model = new RepositoryModelTicket
+                {
+                    ValidFromDate = "01.01.1970",
+                    ValidFromTime = "12:00",
+                    FromStation = "Oslo S",
+                    ToStation = "Trondheim",
+                    Price = 100,
+                    CustomerGivenName = "Ola",
+                    CustomerLastName = "Nordmann",
+                    CustomerNumber = "12345678"
+                };
+            else
+                model = ticketList[ticketList.Count - 1];
 
             return View(model);
         }
