ef2f22e4c890815fc3749743dfc0c1af570054b5
xolmc <s325918@oslomet.no>
2019-10-26 20:45:51 +0200

    Removed unused methods

--- Purchase/Data.Access.Layer/Repositories/Interfaces/IPassengerTypeRepository.cs
+++ Purchase/Data.Access.Layer/Repositories/Interfaces/IPassengerTypeRepository.cs
@@ -6,15 +6,7 @@ namespace Data.Access.Layer.Repositories.Interfaces
 {
     public interface IPassengerTypeRepository
     {
-        RepositoryModelPassengerType GetPassengerTypeTypeById(int id);
-
-        RepositoryModelPassengerType DbToServicePt(DbPassengerType dbPassengerType);
-
         List<RepositoryModelPassengerType> GetAllPt();
-
-        bool AddPT(RepositoryModelPassengerType passengerType);
-
-        bool DeletePT(int id);
-        bool UpdatePt(int id, RepositoryModelPassengerType pt);
+        
     }
 }
