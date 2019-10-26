a7b20e3b93f83d4a18e07262246453a48bab6687
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-26 15:23:43 +0200

    Changed name of ListView and ListController to TicketView and TicketController

---
+++ Purchase/MVC/Controllers/TicketController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Logic.Layer;
+using Microsoft.AspNetCore.Mvc;
+using Model.ViewModels;
+
+namespace MVC.Controllers
+{
+    public class TicketController : Controller
+    {
+        public readonly TicketBLL TicketBll;
+
+        public TicketController(TicketBLL ticketBll)
+        {
+            TicketBll = ticketBll;
+        }
+        
+        public IActionResult Ticket()
+        {
+
+            var ticketList = TicketBll.GetAllTickets();
+            var model = ticketList[ticketList.Count - 1];
+
+            return View(model);
+        }
+    }
+
+}
